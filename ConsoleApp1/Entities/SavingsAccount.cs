10722f12a0d70b05cfcc5de8e6f8ac272653855f
Renan Andrade <renan.automation@gmail.com>
2021-04-25 12:16:03 -0300

    Novas Atualizações

--- ConsoleApp1/Entities/SavingsAccount.cs
+++ ConsoleApp1/Entities/SavingsAccount.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace ConsoleApp1.Entities
 {
-    class SavingsAccount
+    class SavingsAccount : Account
     {
+        public double  InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+
+        }
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number,holder,balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,12 +8,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome");
+            Account acc = new Account(1001,"Alex",0.0);
+            BusinessAccount bacc = new BusinessAccount(1002,"Maria",0.0,500.00);
 
-            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.00, 500.00);
+            // UpCasting
 
-            Console.WriteLine(account.Balance.ToString("F2",CultureInfo.InvariantCulture));
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003,"Bob",0.0,200.0);
+            Account acc3 = new SavingsAccount(1004,"Anna",0.0,0.01);
 
+            // DownCasting
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
         }
     }
 }
