c1e70ebdc2726620f2607205800930df7a7fe537
Renan Andrade <renan.automation@gmail.com>
2021-04-25 11:26:02 -0300

    Adicionei uma mensagem de Welcome

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using ConsoleApp1.Entities;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.00, 500.00);
+
+            Console.WriteLine(account.Balance.ToString("F2",CultureInfo.InvariantCulture));
+
+        }
+    }
+}
